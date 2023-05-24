2df863a106d8ed6a3fc90ef7fa8d96894fabbc9d
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-24 12:51:13 +0200

    Setup hadlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 // SETUP basic server
 const express = require('express')
+const handlebars = require('express-handlebars')
 
 const app = express() //instance of the express server
 
 const PORT = 5000 // Making a constant for the port which server is running to
+
+//Handlebars configurations
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',  //Make it easier to be used 
+}))
+app.set('view engine', 'hbs')
+app.set('views', 'src/views') // To set the directory views because it will look for it in the main folder 
+
+//Router
 app.get('/', (req, res) =>{ //First end point with first handler
-res.send('Hello from express')
+res.render('index')
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`)); // And massage to be sure that server is running without any problems

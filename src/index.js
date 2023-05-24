9d0176e745369c0e8802012fda5c83608f924b0e
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-24 12:17:56 +0200

    Setup Basic Server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+// SETUP basic server
+const express = require('express')
+
+const app = express() //instance of the express server
+
+const PORT = 5000 // Making a constant for the port which server is running to
+app.get('/', (req, res) =>{ //First end point with first handler
+res.send('Hello from express')
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`)); // And massage to be sure that server is running without any problems

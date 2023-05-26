7c9ec2a2c23d67fcd6207d4fd6d01275f369ed62
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-26 09:39:37 +0200

    Add ID generators
    few types to generate ID

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,16 +1,18 @@
 //file for creating cube
-
+//const uniqid = require("uniqid"); // Generate uniq Id but it's necessary to install "npm i uniqid"
 const cubes = [];
 
-exports.getAll = () => cubes.slice()
+exports.getAll = () => cubes.slice();
 
 exports.create = (cubeData) => {
   const newCube = {
+    //id: (new Date()).getTime(), // We can take Id to date
+    //id: uniqid() // Generate uniq Id but it's necessary to install "npm i uniqid"
     id: cubes.length + 1,
     ...cubeData,
   };
-  
-    cubes.push(newCube);
+
+  cubes.push(newCube);
 
   return newCube;
 };
